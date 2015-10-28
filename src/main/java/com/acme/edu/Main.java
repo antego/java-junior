c6563e855066655ee52a47899994786df77a5f8b
antego <antego@users.noreply.github.com>
2015-10-28 17:01:47 +0300

    Added Main and Printer

---
+++ src/main/java/com/acme/edu/Main.java
@@ -0,0 +1,7 @@
+package com.acme.edu;
+
+public class Main{
+	public static void main(String[] args) {
+		com.acme.edu.presentation.Printer.print("Hello, world!");
+	}
+}
---
+++ src/main/java/com/acme/edu/presentation/Printer.java
@@ -0,0 +1,7 @@
+package com.acme.edu.presentation;
+
+public class Printer{
+	public static void print(String m) {
+		System.out.println(m);
+	}
+}
