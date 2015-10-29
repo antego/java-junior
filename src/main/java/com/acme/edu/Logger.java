0a352adb2d3580bde99cef7cd37a642d813d2d1a
antego <antego@users.noreply.github.com>
2015-10-29 22:00:34 +0300

    Add static prefixes and JavaDoc for logging methods

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,20 +1,39 @@
 package com.acme.edu;
 
+
 public class Logger {
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String CHAR_PREFIX = "char: ";
+    /**
+     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
+     * @param message value for logging
+     */
     public static void log(int message) {
-        printInConsole("primitive: " + message);
+        printInConsole(PRIMITIVE_PREFIX + message);
     }
 
+    /**
+     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
+     * @param message value for logging
+     */
     public static void log(byte message) {
-        printInConsole("primitive: " + message);
+        printInConsole(PRIMITIVE_PREFIX + message);
     }
 
+    /**
+     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
+     * @param message value for logging
+     */
     public static void log(boolean message) {
-        printInConsole("primitive: " + message);
+        printInConsole(PRIMITIVE_PREFIX + message);
     }
 
+    /**
+     * Logs input value with prefix {@value #CHAR_PREFIX}
+     * @param message value for logging
+     */
     public static void log(char message) {
-        printInConsole("char: " + message);
+        printInConsole(CHAR_PREFIX + message);
     }
 
     private static void printInConsole(String message) {
