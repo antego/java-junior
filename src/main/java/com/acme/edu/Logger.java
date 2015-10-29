ccd7a816ca2cc0c85603bdb47afd92c605787d65
antego <antego@users.noreply.github.com>
2015-10-29 15:37:13 +0300

    Added method to print message in console

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,18 +2,22 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printInConsole("primitive: " + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printInConsole("primitive: " + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        printInConsole("primitive: " + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        printInConsole("char: " + message);
+    }
+
+    private static void printInConsole(String message) {
+        System.out.println(message);
     }
 }
