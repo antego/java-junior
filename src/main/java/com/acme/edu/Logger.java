2c6d732a3dc50d563ffff60aeb3f963e6e7afd9d
antego <antego@users.noreply.github.com>
2015-10-30 13:51:18 +0300

    Course Project Iteration 02: Logger Library. Iteration 2_1 done.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,42 +1,84 @@
 package com.acme.edu;
 
-
+/**
+ * This class contains static methods to log values of different types.
+ */
 public class Logger {
-    public static final String PRIMITIVE_PREFIX = "primitive: ";
-    public static final String CHAR_PREFIX = "char: ";
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String OBJECT_PREFIX = "reference: ";
+    private static final String STRING_PREFIX = "string: ";
+
+    private static int summ = 0;
+    private static boolean intSetted = false;
+
     /**
-     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
-     * @param message value for logging
+     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}.
+     * @param message value for logging.
      */
     public static void log(int message) {
-        printInConsole(PRIMITIVE_PREFIX + message);
+        printInConsole(PRIMITIVE_PREFIX, message + "", "");
+        if(((long) message + summ) > Integer.MAX_VALUE) {
+            summ = Integer.MAX_VALUE;
+        } else {
+            summ += message;
+        }
+        intSetted = true;
     }
 
-    /**
-     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
-     * @param message value for logging
-     */
-    public static void log(byte message) {
-        printInConsole(PRIMITIVE_PREFIX + message);
-    }
+//    /**
+//     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}.
+//     * @param message value for logging.
+//     */
+//    public static void log(byte message) {
+//        printInConsole(Logger.PRIMITIVE_PREFIX + message);
+//    }
 
     /**
-     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}
-     * @param message value for logging
+     * Logs input value with prefix {@value #PRIMITIVE_PREFIX}.
+     * @param message value for logging.
      */
     public static void log(boolean message) {
-        printInConsole(PRIMITIVE_PREFIX + message);
+        printSumm(summ);
+        printInConsole(PRIMITIVE_PREFIX, message + "", "");
     }
 
     /**
-     * Logs input value with prefix {@value #CHAR_PREFIX}
-     * @param message value for logging
+     * Logs input value with prefix {@value #CHAR_PREFIX}.
+     * @param message value for logging.
      */
     public static void log(char message) {
-        printInConsole(CHAR_PREFIX + message);
+        printSumm(summ);
+        printInConsole(CHAR_PREFIX, message + "", "");
     }
 
-    private static void printInConsole(String message) {
-        System.out.println(message);
+    /**
+     * Logs input value with prefix {@value #STRING_PREFIX}.
+     * @param message value for logging.
+     */
+    public static void log(String message) {
+        printSumm(summ);
+        printInConsole(STRING_PREFIX, message, "");
+    }
+
+    /**
+     * Logs input {@code Object.toString()} with prefix {@value #OBJECT_PREFIX}.
+     * @param message value for logging.
+     */
+    public static void log(Object message) {
+        printSumm(summ);
+        printInConsole(OBJECT_PREFIX, message.toString(), "");
+    }
+
+    private static void printSumm(int s){
+        if(intSetted) {
+            printInConsole("primitive: ",summ + "","");
+        }
+        summ = 0;
+        intSetted = false;
+    }
+
+    private static void printInConsole(String prefix, String message, String postfix) {
+        System.out.println(prefix + message + postfix);
     }
 }
