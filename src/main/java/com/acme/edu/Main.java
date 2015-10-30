96cde546ce8bfb20187951535fb5f242b3d14d46
antego <antego@users.noreply.github.com>
2015-10-30 09:55:27 +0300

    Delete lab1 files

--- src/main/java/com/acme/edu/Main.java
+++
@@ -1,7 +0,0 @@
-package com.acme.edu;
-
-public class Main{
-	public static void main(String[] args) {
-		com.acme.edu.presentation.Printer.print("Hello, world!");
-	}
-}
--- src/main/java/com/acme/edu/presentation/Printer.java
+++
@@ -1,7 +0,0 @@
-package com.acme.edu.presentation;
-
-public class Printer{
-	public static void print(String m) {
-		System.out.println(m);
-	}
-}
