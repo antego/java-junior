03191fd16005423e1d81382be5b42a5f0d7daafd
antego <antego@users.noreply.github.com>
2015-11-03 17:14:00 +0300

    Iteration 03. OOD. Unit testing. Added before to unit tests.

--- src/test/java/com/acme/edu/unit/LoggerTest.java
+++ src/test/java/com/acme/edu/unit/LoggerTest.java
@@ -3,57 +3,58 @@ package com.acme.edu.unit;
 
 import com.acme.edu.*;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 
 public class LoggerTest {
+    State state;
+    Logger logger;
+
+    @Before
+    public void setUpTest() {
+        state = mock(State.class);
+        when(state.giveMeHasIntState()).thenReturn(state);
+        when(state.giveMeHasStringState()).thenReturn(state);
+        when(state.giveMeBlankState()).thenReturn(state);
+        logger = new Logger(state);
+    }
+
     @Test
     public void shouldPrintSumOfIntegers() {
-        //region given
-        State state = mock(State.class);
-        when(state.giveMeHasIntState()).thenReturn(state);
-        Logger logger = new Logger(state);
-        //endregion
-
         //region when
         logger.log(1);
         logger.stopLogging();
         //endregion
 
+        //region then
         verify(state).processMessage("1");
+        //endregion
     }
 
     @Test
     public void shouldPrintSumOfStrings() {
-        //region given
-        State state = mock(State.class);
-        when(state.giveMeHasStringState()).thenReturn(state);
-        Logger logger = new Logger(state);
-        //endregion
-
         //region when
         logger.log("String");
         logger.stopLogging();
         //endregion
 
+        //region then
         verify(state).processMessage("String");
+        //endregion
     }
 
     @Test
     public void shouldPrintSumOf() {
-        //region given
-        State state = mock(State.class);
-        when(state.giveMeBlankState()).thenReturn(state);
-        Logger logger = new Logger(state);
-        //endregion
-
         //region when
         logger.log(new int[][]{{0}});
         logger.stopLogging();
         //endregion
 
+        //region then
         verify(state).processMessage("primitives array: {" + Logger.SEP + "{0}" + Logger.SEP + "}");
+        //endregion
     }
 }
--- src/test/java/com/acme/edu/unit/StatesTest.java
+++ src/test/java/com/acme/edu/unit/StatesTest.java
@@ -4,23 +4,31 @@ import com.acme.edu.BlankState;
 import com.acme.edu.HasIntState;
 import com.acme.edu.HasStringState;
 import com.acme.edu.Printer;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class StatesTest {
+
+    private Printer printer;
+
+    @Before
+    public void setUpTests() {
+        printer = mock(Printer.class);
+    }
+
     @Test
     public void hasIntStateTest() {
         //region given
-        Printer printer = mock(Printer.class);
-        HasIntState state = new HasIntState(printer);
+        HasIntState hasIntState = new HasIntState(printer);
         //endregion
 
         //region when
-        state.processMessage("1");
-        state.processMessage("1");
-        state.flushBuffer();
+        hasIntState.processMessage("1");
+        hasIntState.processMessage("1");
+        hasIntState.flushBuffer();
         //endregion
 
         verify(printer).print("primitive: 2");
@@ -29,14 +37,13 @@ public class StatesTest {
     @Test
     public void hasStringStateTest() {
         //region given
-        Printer printer = mock(Printer.class);
-        HasStringState state = new HasStringState(printer);
+        HasStringState hasStringState = new HasStringState(printer);
         //endregion
 
         //region when
-        state.processMessage("testString");
-        state.processMessage("testString");
-        state.flushBuffer();
+        hasStringState.processMessage("testString");
+        hasStringState.processMessage("testString");
+        hasStringState.flushBuffer();
         //endregion
 
         verify(printer).print("string: testString (x2)");
@@ -45,13 +52,12 @@ public class StatesTest {
     @Test
     public void BlankStateTest() {
         //region given
-        Printer printer = mock(Printer.class);
-        BlankState state = new BlankState(printer);
+        BlankState blankState = new BlankState(printer);
         //endregion
 
         //region when
-        state.processMessage("char: f");
-        state.flushBuffer();
+        blankState.processMessage("char: f");
+        blankState.flushBuffer();
         //endregion
 
         verify(printer).print("char: f");
