3cc0049527ccb86cda2e52169ef94ced8f0003ab
antego <antego@users.noreply.github.com>
2015-11-04 10:35:00 +0300

    Logger Library. JavaDoc for Decorator related classes.

--- src/main/java/com/acme/edu/Decorator.java
+++ src/main/java/com/acme/edu/Decorator.java
@@ -1,6 +1,15 @@
 package com.acme.edu;
 
-//@FunctionalInterface
+/**
+ * Decorator that decorates input message
+ */
+@FunctionalInterface
 public interface Decorator {
+    /**
+     * Decorates message by specified rules.
+     *
+     * @param message Message to decorate
+     * @return Decorated message
+     */
     String decorateMessage(String message);
 }
--- src/main/java/com/acme/edu/Decorators.java
+++ src/main/java/com/acme/edu/Decorators.java
@@ -12,46 +12,91 @@ public class Decorators implements DecoratorFactory {
     private static Decorator intTwoDimensionalArrayDecorator = s -> "primitives matrix: " + s;
     private static Decorator intFourDimensionalArrayDecorator = s -> "primitives multimatrix: " + s;
 
+    /**
+     * Returns dummy decorator
+     *
+     * @return Decorator which does nothing with the message.
+     */
     @Override
     public Decorator getDummyDecorator() {
         return dummyDecorator;
     }
 
+    /**
+     * Returns decorator for integer messages.
+     *
+     * @return decorator that adds "primitive" prefix.
+     */
     @Override
     public Decorator getIntDecorator() {
         return intDecorator;
     }
 
+    /**
+     * Returns decorator for string messages.
+     *
+     * @return decorator that adds "string" prefix.
+     */
     @Override
     public Decorator getStringDecorator() {
         return stringDecorator;
     }
 
+    /**
+     * Returns decorator for char messages.
+     *
+     * @return decorator that adds "char" prefix.
+     */
     @Override
     public Decorator getCharDecorator() {
         return charDecorator;
     }
 
+    /**
+     * Returns decorator for boolean messages.
+     *
+     * @return decorator that adds "primitive" prefix.
+     */
     @Override
     public Decorator getBoolDecorator() {
         return boolDecorator;
     }
 
+    /**
+     * Returns decorator for reference messages.
+     *
+     * @return decorator that adds "reference" prefix.
+     */
     @Override
     public Decorator getObjectDecorator() {
         return objectDecorator;
     }
 
+    /**
+     * Returns decorator for integer array messages.
+     *
+     * @return decorator that adds "primitives array" prefix.
+     */
     @Override
     public Decorator getIntArrayDecorator() {
         return intArrayDecorator;
     }
 
+    /**
+     * Returns decorator for integer matrix messages.
+     *
+     * @return decorator that adds "primitives matrix" prefix.
+     */
     @Override
     public Decorator getIntTwoDimensionalArrayDecorator() {
         return intTwoDimensionalArrayDecorator;
     }
 
+    /**
+     * Returns decorator for four-dimensional integer array messages.
+     *
+     * @return decorator that adds "primitives multimatrix" prefix.
+     */
     @Override
     public Decorator getIntFourDimensionalArrayDecorator() {
         return intFourDimensionalArrayDecorator;
